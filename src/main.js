82a51571896785bbb9911fdd3a45ca1bc97090bc
我叫mang <18897985375@163.com>
2020-10-03 10:31:59 +0800

    1.第一次提交

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less' //加载全局样文件
 
 Vue.config.productionTip = false
 
+
+
+//创建 vue 根实例
+//把router配置到根实例中
+//通过render方法把 App根组件渲染到render
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app')   //el:'#app' //等价于 $mount('#app')
+
+
